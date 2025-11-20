bfff270fe36a1e527b5fef6a45e775db2433d0aa
Wawawaongaku <umisorasannobotto@gmail.com>
2025-11-20 22:21:47 +0900

    yosi

--- extension.js
+++ extension.js
@@ -1,27 +1,73 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import GLib from "gi://GLib";
+import Meta from "gi://Meta";
+import Shell from "gi://Shell";
+import St from "gi://St";
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
-export default class PlainExampleExtension extends Extension {
+export default class BackgroundWindowExtension extends Extension {
+    constructor(metadata) {
+        super(metadata);
+        this._actor = null;
+        this._label = null;
+        this._sourceId = 0;
+        this._seconds = 0;
+    }
+
     enable() {
-        
+        try {
+            this._seconds = 0;
+            this._actor = new St.BoxLayout({
+                style_class: "backgroundwindow-demo",
+                vertical: true,
+                reactive: false,
+            });
+
+            this._actor.set_position(24, 24);
+
+            this._label = new St.Label({
+                text: "BackgroundWindow ready",
+                reactive: false,
+            });
+
+            this._actor.add_child(this._label);
+            Main.layoutManager.addChrome(this._actor);
+
+            this._sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, () => {
+                this._seconds += 1;
+                if (this._label) {
+                    const windows = global.display.get_tab_list(Meta.TabList.NORMAL, null);
+                    this._label.text = `ループ: ${this._seconds} 秒`;
+                    windows.forEach(window => {
+                        
+                    });
+                }
+                return GLib.SOURCE_CONTINUE;
+            });
+            
+        } catch (error) {
+            throw error;
+        }
     }
 
     disable() {
+        try {
+            
+            if (this._sourceId) {
+                GLib.source_remove(this._sourceId);
+                this._sourceId = 0;
+            }
+
+            if (this._actor) {
+                Main.layoutManager.removeChrome(this._actor);
+                this._actor.destroy();
+                this._actor = null;
+            }
+
+            this._label = null;
+            this._seconds = 0;
+            
+        } catch (error) {
+        }
     }
 }
