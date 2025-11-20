77949c2f2dfe21cdfc22da5b4fce7b5eebfcf043
Wawawaongaku <umisorasannobotto@gmail.com>
2025-11-21 01:49:53 +0900

    selected                            の挙動確認

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import Clutter from "gi://Clutter";
 import GLib from "gi://GLib";
 import Meta from "gi://Meta";
 import Shell from "gi://Shell";
@@ -33,18 +34,33 @@ export default class BackgroundWindowExtension extends Extension {
             this._actor.add_child(this._label);
             Main.layoutManager.addChrome(this._actor);
 
+            // キーバインディングを一度だけ追加
+            Main.wm.addKeybinding(
+                'toggle-background-window',
+                this.getSettings(),
+                Meta.KeyBindingFlags.NONE,
+                Shell.ActionMode.NORMAL,
+                () => {
+                    const focusedWindow = global.display.get_focus_window();
+                    if (focusedWindow && this._label) {
+                        this._label.text = `Selected window: ${focusedWindow.get_wm_class()}`;
+                    }
+                }
+            );
+
             this._sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, () => {
                 this._seconds += 1;
                 if (this._label) {
                     const windows = global.display.get_tab_list(Meta.TabList.NORMAL, null);
-                    this._label.text = `ループ: ${this._seconds} 秒`;
+                    const window_list = []
                     windows.forEach(window => {
-                        
+                        window_list.push(window.get_wm_class());
                     });
+                    this._label.text = `ウィンドウ: ${window_list.join(", ")}`
                 }
                 return GLib.SOURCE_CONTINUE;
             });
-            
+
         } catch (error) {
             throw error;
         }
@@ -52,7 +68,9 @@ export default class BackgroundWindowExtension extends Extension {
 
     disable() {
         try {
-            
+            // キーバインディングを削除
+            Main.wm.removeKeybinding('toggle-background-window');
+
             if (this._sourceId) {
                 GLib.source_remove(this._sourceId);
                 this._sourceId = 0;
@@ -66,7 +84,7 @@ export default class BackgroundWindowExtension extends Extension {
 
             this._label = null;
             this._seconds = 0;
-            
+
         } catch (error) {
         }
     }
